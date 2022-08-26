54032d9d8f4276196372980077d8ab8ce59aad29
Edward <s3844470@student.rmit.edu.au>
2022-08-26 17:49:38 +1000

    delete profile clears local storage -need to add a redirect to home

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -17,16 +17,35 @@ function Profile() {
     const name = localStorage.getItem('name');
     const date = localStorage.getItem("date");
 
+    function deleteProfile() {
+        localStorage.clear()
+        window.alert("Profile Deleted")
+    }
+
+    function deleteConfirmation() {
+        if (window.confirm("Are you sure you want to delete your profile?")){
+            deleteProfile();
+        }
+
+        
+    } 
+    
+
+
     return (
         <body>
             <h1>Profile Page</h1>
             <p id = "user_name">{name}</p>
             <p id = "user_email">{email}</p>
             <p id = "join_date">{date}</p>   
+
             <form action="/ProfileEdit">
             <input type="submit" value="Edit Profile" />
             </form>
-            <button>Delete</button>     
+
+            <button onClick={deleteConfirmation}>Delete Profile</button>
+            
+
 
         </body>
 
